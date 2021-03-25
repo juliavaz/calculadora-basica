30de8482e4c8e7d23d86d1c2f14ac10173d5c992
Leandro Faria <leandro@leandrofaria.com>
2021-03-25 20:10:42 -0300

    Updates

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -3,3 +3,28 @@ document.addEventListener('DOMContentLoaded', () => {
 
   // here goes the logical
 });
+
+let press = (key) => {
+  if (typeof(key) === 'number') {
+    updateDisplay(key, false);
+  } else {
+    switch (key) {
+      case 'clear':
+        clearDisplay();
+    }
+  }
+}
+
+let updateDisplay = (newText, clear) => {
+  let display = document.getElementById('display');
+  if (clear === true) {
+    display.innerText = newText;
+  } else {
+    display.innerText = display.innerText += newText;
+  }
+}
+
+let clearDisplay = () => {
+  let display = document.getElementById('display');
+  display.innerText = '';
+}
