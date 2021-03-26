f8ff3b64081bb71e6ad6e9e89daaf9d728b50522
Mooooooov <mateusvasconcelos4210@gmail.com>
2021-03-26 06:11:32 -0300

    redesigning the front of the page

--- keycode.js
+++ keycode.js
@@ -1,37 +1,37 @@
 const teclas = (e) => {
-    if (e.keyCode === 96 || e.keyCode === 48){
-        //numero 0
-    }else if(e.keyCode === 97 || e.keyCode === 49){
-        //numero 1
-    }else if(e.keyCode === 98 || e.keyCode === 50){
-        //numero 2
-    }else if(e.keyCode === 99 || e.keyCode === 51){
-        //numero 3
-    }else if(e.keyCode === 100 || e.keyCode === 52){
-        //numero 4
-    }else if(e.keyCode === 101 || e.keyCode === 53){
-        //numero 5
-    }else if(e.keyCode === 102 || e.keyCode === 54){
-        //numero 6
-    }else if(e.keyCode ===  103|| e.keyCode === 55){
-        //numero 7
-    }else if(e.keyCode === 104 || e.keyCode === 56){
-        //numero 8
-    }else if(e.keyCode === 105 || e.keyCode === 57){
-        //numero 9
-    }else if(e.keyCode === 106 || e.keyCode === 56){
-        // asterisco *
-    }else if(e.keyCode === 111 || e.keyCode === 81){
-        // divisão /
-    }else if(e.keyCode === 109 || e.keyCode === 189){
-        // subtração -
-    }else if(e.keyCode === 107 || e.keyCode === 187){
-        // adição +
-    }else if(e.keyCode === 13){
-        // enter 
-    }else if(e.keyCode === 190 || e.keyCode === 110 || e.keyCode === 188){
-        // ponto .
-    }else if(e.keyCode === 8){
-        // backspace - apagar
-    }
+  if (e.keyCode === 96 || e.keyCode === 48) {
+    //numero 0
+  } else if (e.keyCode === 97 || e.keyCode === 49) {
+    //numero 1
+  } else if (e.keyCode === 98 || e.keyCode === 50) {
+    //numero 2
+  } else if (e.keyCode === 99 || e.keyCode === 51) {
+    //numero 3
+  } else if (e.keyCode === 100 || e.keyCode === 52) {
+    //numero 4
+  } else if (e.keyCode === 101 || e.keyCode === 53) {
+    //numero 5
+  } else if (e.keyCode === 102 || e.keyCode === 54) {
+    //numero 6
+  } else if (e.keyCode === 103 || e.keyCode === 55) {
+    //numero 7
+  } else if (e.keyCode === 104 || e.keyCode === 56) {
+    //numero 8
+  } else if (e.keyCode === 105 || e.keyCode === 57) {
+    //numero 9
+  } else if (e.keyCode === 106 || e.keyCode === 56) {
+    // asterisco *
+  } else if (e.keyCode === 111 || e.keyCode === 81) {
+    // divisão /
+  } else if (e.keyCode === 109 || e.keyCode === 189) {
+    // subtração -
+  } else if (e.keyCode === 107 || e.keyCode === 187) {
+    // adição +
+  } else if (e.keyCode === 13) {
+    // enter
+  } else if (e.keyCode === 190 || e.keyCode === 110 || e.keyCode === 188) {
+    // ponto .
+  } else if (e.keyCode === 8) {
+    // backspace - apagar
+  }
 };
