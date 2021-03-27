5b9f24b9ead821daecf46474e7a1d2ba8ee6965e
Julia Vaz <juliavaz.dev@gmail.com>
2021-03-26 22:28:33 -0300

    funcionando

--- src/js/calculadora.js
+++ src/js/calculadora.js
@@ -43,48 +43,49 @@ let loadHistory = (exp) => {
   updateDisplay(exp, true);
 };
 
-
 let addKey  = (event) => {  
   let display = document.getElementById('display');
   let code = event.keyCode;
   let char = String.fromCharCode(code);
   
   if(char == "0"){   
-    updateDisplay(char, true);
+    press(char, false);
   } else if (char == "1" || code == 49) {
-    updateDisplay(char, true);
+    press(char, false);
   } else if (char == "2" || code == 50) {
-    updateDisplay(char, true);
+    press(char, false);
   } else if (char == "3" || code == 51) {
-    updateDisplay(char, true);
+    press(char, false);
   } else if (char == "4" || code == 52) {
-    updateDisplay(char, true);
+    press(char, false);
   } else if (char == "5" || code == 53) {
-    updateDisplay(char, true);
+    press(char, false);
   } else if (char == "6" || code == 54) {
-    updateDisplay(char, true);
+    press(char, false);
   } else if (char == "7" || code == 55) {
-    updateDisplay(char, true);
+    press(char, false);
   } else if (char == "8" || code == 56) {
-    updateDisplay(char, true);
+    press(char, false);
   } else if (char == "9" || code == 57) {
-    updateDisplay(char, true);
+    press(char, false);
   } else if (char == "*" || code == 42) {
-    updateDisplay(char, true);
+    press(char, false);
   } else if (char == "+" || code == 43 ) {
-    updateDisplay(char, true);
+    press(char, false);
   } else if (char == "-" || code == 45) {
-    updateDisplay(char, true);
+    press(char, false);
   } else if (char == "." || code == 46) {
-    updateDisplay(char, true);
+    press(char, false);
   } else if (char == "/" || code == 47) {
-    updateDisplay(char, true);
+    press(char, false);
   } else if (char == "=" || code == 61) {
-    updateDisplay(char, true);
-  } else if (char == "backspace" || code == 8) {
-    updateDisplay(char, true);
+    press(char, false);
+  } else if (char == "Backspace" || code == 8) {
+    press("clear" , false);
   } else if (char == "Enter" || code == 13) {
-    alert("vc apertou enter");
-  } 
+    press("=", false);
+  } else {
+    display.innerText = display.innerText += key;
+  }
 }
 document.addEventListener("keypress", addKey);
